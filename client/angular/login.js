df9a3d8229a4290259b510cb467da31a9fc243cb
Akash <akash.1210017@kiet.edu>
2016-08-18 07:01:33 +0530

    added login module.

---
+++ client/angular/login.js
@@ -0,0 +1,64 @@
+var app = angular.module('login',[]);
+app.controller('registerAction',function($scope,$http,$window){
+
+  $scope.schoolRegister = function(){
+
+      var school =JSON.stringify($scope.register);
+
+
+      $http({
+
+
+               method : 'POST',
+               url : 'http://localhost:3000/api/Schools/registerSchool',
+               headers: {'Content-Type': 'application/json'},
+               data : school
+           }).
+           success(function(data,status,headers,config){
+                document.getElementById("registerSubmit").disabled = "true";
+                document.getElementById("registerResponse").innerHTML = "<strong>Thank You !</strong> Email has been sent to verify. ";
+
+                return;
+
+           })
+      .error(function(data,status,headers,config){
+
+
+        document.getElementById("registerResponse").innerHTML = "<span style='color:red'>*"+data.error.message+"</span>";
+    });
+
+  }
+
+});
+
+
+app.controller('loginAction',function($scope,$http,$window){
+  $scope.schoolLogin = function(){
+      var school =JSON.stringify($scope.login);
+
+
+      $http({
+
+
+               method : 'POST',
+               url : 'http://localhost:3000/api/Schools/loginSchool',
+               headers: {'Content-Type': 'application/json'},
+               data : school
+           }).
+           success(function(data,status,headers,config){
+                document.getElementById("loginSubmit").disabled = "true";
+                document.getElementById("loginResponse").innerHTML = "<strong>Thank You !</strong> Email has been sent to verify. ";
+
+                return;
+
+           })
+      .error(function(data,status,headers,config){
+
+
+        document.getElementById("loginResponse").innerHTML = "<span style='color:red'>"+data.error.message;
+        return;
+    });
+
+  }
+
+});
